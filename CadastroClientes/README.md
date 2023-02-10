c85966dd72a1154f19894a98a8bbc4b67e1c5912
copli <110202409+copli@users.noreply.github.com>
2023-02-10 09:28:04 -0300

    Add files via upload

---
+++ CadastroClientes/README.md
@@ -0,0 +1,2 @@
+# CadastroClientes
+UC12 - SA2 - Sistema Cadastro de Clientes
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using CadastroClientes.Classes;
+
+// Método construtor
+PessoaFisica novaPf = new PessoaFisica();
+novaPf.nome = "Adriano";
+novaPf.cpf = 01234567890;
+novaPf.dataNasc = new DateTime(2001, 01, 01);
+novaPf.endereco = "Rua Niteroi, 1532";
+novaPf.rendimento = 12000.00f;
+
+Console.WriteLine(novaPf.nome);
+Console.WriteLine(novaPf.cpf);
+Console.WriteLine(novaPf.dataNasc);
+Console.WriteLine(novaPf.endereco);
+Console.WriteLine(novaPf.rendimento);
+
+// Concatenação
+Console.WriteLine("Nome do Cliente: " + novaPf.nome);
+
+// Interpolação
+Console.WriteLine($"Nome do Cliente: {novaPf.nome} - CPF: {novaPf.cpf}");
